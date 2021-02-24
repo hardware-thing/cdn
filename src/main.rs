926f7949ea0f1e464ceb16151297fce9f47f9822
Michal Janočko <michal@janocko.com>
2021-02-23 19:14:19 -0600

    :sparkles: Serving CSS and lists of components

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate rocket;
 
 use rocket::{http::ContentType, response::Response, State};
 use std::{
+    io::Cursor,
     sync::{Arc, RwLock},
     thread,
 };
@@ -13,12 +14,44 @@ mod cache;
 mod watcher;
 
 #[get("/css?<components>")]
-fn css(components: String, _cache: State<'_, Cache>) -> Response {
-    let _files: Vec<String> = components
+fn css(components: String, cache: State<'_, Cache>) -> Response {
+    let files: Vec<String> = components
         .split("-")
         .map(|file| file.replace(":", "/").to_string())
         .collect();
-    Response::build().header(ContentType::CSS).finalize()
+    let mut css = String::new();
+
+    if let Ok(lock) = cache.try_read() {
+        for file in files {
+            css += (*lock)
+                .get(&file)
+                .map(|content| content.as_str())
+                .unwrap_or("");
+        }
+    }
+
+    Response::build()
+        .header(ContentType::CSS)
+        .sized_body(css.len(), Cursor::new(css))
+        .finalize()
+}
+
+#[get("/list")]
+fn list(cache: State<'_, Cache>) -> String {
+    match cache.try_read() {
+        Ok(lock) => {
+            let mut keys = (*lock)
+                .keys()
+                .map(|k| k.replace("/", ":").to_owned())
+                .collect::<Vec<String>>();
+            keys.sort();
+            keys.join("\n")
+        }
+        Err(_) => {
+            println!("Could not acquire lock on cache");
+            String::new()
+        }
+    }
 }
 
 #[launch]
@@ -29,5 +62,7 @@ fn rocket() -> rocket::Rocket {
     let watcher_cache = cache.clone();
     thread::spawn(move || watcher::watch(watcher_cache));
 
-    rocket::ignite().mount("/", routes![css]).manage(cache)
+    rocket::ignite()
+        .mount("/", routes![css, list])
+        .manage(cache)
 }
